9eb1a4d29a88f775a2348810268341071be275ac
Даир Байдаулетов <dair.baidauletov@gmail.com>
2018-02-01 15:49:28 +0200

    Final version with build and rar

--- www/js/index.js
+++ www/js/index.js
@@ -30,8 +30,7 @@ function getImage(selection) {
 
 function setOptions(srcType) {
     var options = {
-        // Some common settings are 20, 50, and 100
-        quality: 50,
+        quality: 100,
         destinationType: Camera.DestinationType.FILE_URI,
         // In this app, dynamically set the picture source, Camera or photo gallery
         sourceType: srcType,
