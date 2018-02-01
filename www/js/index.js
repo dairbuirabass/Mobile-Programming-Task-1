0ce8a0b34cd3afb54661cf737e96255eea253c4e
Даир Байдаулетов <dair.baidauletov@gmail.com>
2018-02-01 13:29:32 +0200

    Working www files

---
+++ www/js/index.js
@@ -0,0 +1,48 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+document.getElementById("takePictureBtn").onclick = takePicture;
+document.getElementById("pickPictureBtn").onclick = pickPicture;
+
+var srcType;
+var options;
+
+function onDeviceReady() {}
+
+function takePicture() {
+  srcType = Camera.PictureSourceType.CAMERA;
+  options = setOptions(srcType);
+  getImage();
+}
+
+function pickPicture() {
+  srcType = Camera.PictureSourceType.SAVEDPHOTOALBUM;
+  options = setOptions(srcType);
+  getImage();
+}
+
+function getImage(selection) {
+  navigator.camera.getPicture(function cameraSuccess(imageUri) {
+    displayImage(imageUri);
+  }, function cameraError(error) {
+    console.debug("Unable to obtain picture: " + error, "app");
+  }, options);
+}
+
+
+function setOptions(srcType) {
+    var options = {
+        // Some common settings are 20, 50, and 100
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery
+        sourceType: srcType,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: Camera.MediaType.PICTURE,
+        correctOrientation: true  //Corrects Android orientation quirks
+    }
+    return options;
+}
+
+function displayImage(imgUri) {
+    var imageElement = document.getElementById("displayedImage");
+    imageElement.src = imgUri;
+}
